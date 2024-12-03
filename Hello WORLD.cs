120fdcbd4898f161409e4ebc9e47111273e49c8f
broat150 <thvi1202@montessori-palau.net>
2024-12-03 20:06:34 +0100

    this file is hello mom

---
+++ Hello WORLD.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWORLD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("¿YOUR MOM?");
+        print("yeeees");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
